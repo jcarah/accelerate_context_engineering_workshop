314dd8f3d4ed7b4bf7c86b565b27752af3892666
Dani Zamora <iamdanielazamora@gmail.com>
2026-01-07 22:12:34 +0000

    adding multiple agents to our repo to pick from one (easiest)

--- customer_service/customer_service/tools/__init__.py
+++ customer_service/customer_service/tools/__init__.py
@@ -11,19 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""Includes all shared libraries for the agent."""
-
-import os
-
-import google.auth
-
-_, project_id = google.auth.default()
-os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
-os.environ["GOOGLE_CLOUD_LOCATION"] = "global"
-os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
-
-from . import agent
-
-
-__all__ = ["agent"]
